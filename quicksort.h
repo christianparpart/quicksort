4f8b572f30467a482ee53a1fd935cab2095b0305
Christian Parpart <christian@parpart.family>
2019-06-12 08:20:47 +0200

    fixes recursion counting; adds customizing pivot selector

--- quicksort.h
+++ quicksort.h
@@ -9,29 +9,37 @@
 #include <utility>
 #include <iterator>
 #include <cstddef>
+#include <cassert>
 
 namespace quicksort {
 
 struct AlgorithmStats {
-	std::size_t recursionDepth = 0;
+	std::size_t callCount = 0;
 	std::size_t swapCount = 0;
 	std::size_t iterationCount = 0;
+	std::size_t recursionDepth = 0;
+	std::size_t currentRecursionDepth = 0;
 };
 
 inline std::ostream& operator<<(std::ostream& out, AlgorithmStats const& stats)
 {
-	out << "{recursionDepth: " << stats.recursionDepth
-		<< ", swaps: " << stats.swapCount
+	out << "{swaps: " << stats.swapCount
 		<< ", iterations, " << stats.iterationCount
+		<< ", recursion: " << stats.recursionDepth
+		<< ", calls: " << stats.callCount
 		<< "}";
 	return out;
 }
 
-template <typename Container>
-auto partition(Container& p, std::size_t low, std::size_t high, AlgorithmStats& stats) -> std::size_t
+// various pivot selector implementations
+size_t pivotNaive(std::size_t low, std::size_t high) { return high; }
+size_t pivotMiddle(std::size_t low, std::size_t high) { return (low + high) / 2; }
+
+template <typename Container, typename Pivot>
+auto partition(Container& p, std::size_t low, std::size_t high, Pivot selectPivot, AlgorithmStats& stats) -> std::size_t
 {
 	auto i = low - 1;
-	auto const pivot = p[high];
+	auto const pivot = selectPivot(low, high); //p[high];
 	auto const incAndSwap = [&](std::size_t j) {
 		++i;
 		std::swap(p[i], p[j]);
@@ -47,17 +55,23 @@ auto partition(Container& p, std::size_t low, std::size_t high, AlgorithmStats& 
 	return i;
 }
 
-template <typename Container>
-void quicksort(Container& p, std::size_t low, std::size_t high, AlgorithmStats& stats)
+template <typename Container, typename Pivot>
+void quicksort(Container& p, std::size_t low, std::size_t high, Pivot pivot, AlgorithmStats& stats)
 {
-	stats.recursionDepth++;
+	stats.callCount++;
+	stats.currentRecursionDepth++;
+	if (stats.recursionDepth < stats.currentRecursionDepth)
+		stats.recursionDepth = stats.currentRecursionDepth;
+
 	if (low < high)
 	{
-		auto const pi = partition(p, low, high, stats);
+		auto const pi = partition(p, low, high, pivot, stats);
 		if (pi > 0)
-			quicksort(p, low, pi - 1, stats);
-		quicksort(p, pi + 1, high, stats);
+			quicksort(p, low, pi - 1, pivot, stats);
+		quicksort(p, pi + 1, high, pivot, stats);
 	}
+
+	stats.currentRecursionDepth--;
 }
 
 template <typename Container>
@@ -65,7 +79,7 @@ auto sort(Container& p) -> AlgorithmStats
 {
 	AlgorithmStats stats;
 	if (auto size = std::size(p); size != 0)
-		quicksort(p, 0, size - 1, stats);
+		quicksort(p, 0, size - 1, pivotMiddle, stats);
 	return stats;
 }
 
